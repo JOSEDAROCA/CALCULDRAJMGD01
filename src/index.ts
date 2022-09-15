e1b46496c9514ea03c7045594e4f8a300f3e6fcd
JOSE MARIA GARCIA DAROCA <110504961+JOSEDAROCA@users.noreply.github.com>
2022-09-15 16:17:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import "./styles.css";
+//Ingres de numeros
+let num1:number = document.getElementById("dato1");
+let num2:number = document.getElementById("dato2");
+
+//Boton
+let btnSum = document.getElementById("btnSum");
+//Escuchando click del Boton
+btnSum.addEventListener("click", () => {
+  let num1: number = document.getElementById(dato1.value);
+  let num2: number = document.getElementById(dato2.value;
+
+}
+  
+
+  console.log(resultado);
+  resultado.innerHTML = (resultado);
